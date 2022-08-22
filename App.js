8740e7d5137b3a43447d68d240d60b8676914374
A Jez <andy@kairosxr.com>
2022-08-21 17:02:51 -0700

    email and password input fields on login screen

--- App.js
+++ App.js
@@ -6,10 +6,16 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
 
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: "#2C6BED" },
+  headerTintStyle: { color: "white" },
+  headerTintColor: "white",
+};
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
         <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,62 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, KeyboardAvoidingView } from "react-native";
+import React, { useState } from "react";
+import { Button, Input, Image } from "react-native-elements";
+import { StatusBar } from "expo-status-bar";
 
 const LoginScreen = () => {
+  const { email, setEmail } = useState("");
+  const { password, setPassword } = useState("");
+
+  const signIn = () => {};
+
   return (
-    <View>
-      <Text>I am the login screen</Text>
-    </View>
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={{
+          uri: "https://upload.wikimedia.org/wikipedia/commons/4/4f/Signal_Blue_Icon.png",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autoFocus
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+
+      <Button containerStyle={styles.button} onPress={signIn} title="Login" />
+      <Button containerStyle={styles.button} type="outline" title="Register" />
+      <View style={{ height: 150 }} />
+    </KeyboardAvoidingView>
   );
 };
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    width: 200,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "white",
+  },
+});
